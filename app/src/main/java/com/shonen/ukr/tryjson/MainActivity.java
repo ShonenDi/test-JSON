93299f0b95a7f1c0a4b0083a4b45d3e9af4074ef
Dima Usatyuk <Shonen1988@gmail.com>
2020-06-10 16:01:11 +0300

    lets start

--- app/src/main/java/com/shonen/ukr/tryjson/MainActivity.java
+++ app/src/main/java/com/shonen/ukr/tryjson/MainActivity.java
@@ -2,13 +2,32 @@ package com.shonen.ukr.tryjson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static String JSON_DATA_URL = "https://jsonplaceholder.typicode.com/posts";
+    private TextView txtData;
+    private Button btbGetData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public URL buildUrlFromUri(String ureUri) throws MalformedURLException {
+        Uri buildUri = Uri.parse(JSON_DATA_URL).buildUpon().build();
+        URL url=null;
+        url = new URL(buildUri.toString());
+        return url;
     }
 }
