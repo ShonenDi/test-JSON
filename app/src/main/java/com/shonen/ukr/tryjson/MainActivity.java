4690c99ec917d67ab1b0b003162db5f72b13b62b
Dima Usatyuk <Shonen1988@gmail.com>
2020-06-11 15:01:48 +0300

    got some little problem here

--- app/src/main/java/com/shonen/ukr/tryjson/MainActivity.java
+++ app/src/main/java/com/shonen/ukr/tryjson/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -53,20 +56,28 @@ public class MainActivity extends AppCompatActivity {
 
     public String httpConnectionResponse(URL url) throws IOException {
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-        String data= "";
+        String result = "";
+        String data = "";
+        String parseData = "";
         try {
             InputStream in = httpURLConnection.getInputStream();
             BufferedReader bf = new BufferedReader(new InputStreamReader(in));
-            String line = "";
-            
-            while (line!=null){
-                line = bf.readLine();
-                data = data + line;
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            while ((line = bf.readLine())!=null){
+                sb.append(line + "\n");
+                data = sb.toString();
+                result = getSingJsonObject(data);
+                parseData = parseData + result +"\n";
             }
+        } catch (JSONException e) {
+            e.printStackTrace();
         } finally {
             httpURLConnection.disconnect();
+
         }
-        return data;
+        return parseData;
     }
 
     public class GetJsonData extends AsyncTask<URL,Void,String>{
@@ -77,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
             String jsonData = "";
             try {
                jsonData = httpConnectionResponse(url);
-                
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -86,7 +96,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String jsonData) {
-            txtData.append(jsonData +"\\n");
+            txtData.append(jsonData +"\n");
         }
+    }
+    public static String getSingJsonObject(String result) throws JSONException {
+        String jsonObj = "";
+        JSONObject userPost = new JSONObject(result);
+        int useNumberId = userPost.getInt("id");
+        String userPostTitle = userPost.getString("title");
+        String userPostBody = userPost.getString("body");
+
+        jsonObj = "User Id: " + useNumberId + "\n"
+                + "Title: " + userPostTitle + "\n"
+                + "Post body: " + userPostBody + "\n";
+        return jsonObj;
     }
 }
